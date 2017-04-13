271f604397ce746e190e936c703aa14650ad27c6
arielkreyes <ariel.k.reyes@hotmail.com>
2017-04-12 23:46:55 -0700

    0412
    Night Shift

--- wp-content/themes/monster23/index.php
+++ wp-content/themes/monster23/index.php
@@ -9,8 +9,10 @@
   ?>
 
       <article <?php post_class(); ?>>
+        <a href="http://placehold.it"><img src="http://placehold.it/350x300"></a>
+        <div class="info-wrap">
           <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-          <?php the_post_thumbnail('thumbnail'); ?>
+
           <div class="entry-content">
             <?php if(is_singular()){
               //single post, page, attachment, etc
@@ -25,10 +27,11 @@
               the_excerpt();
             }//end of else  ?>
         </div>
-        <div class="postmeta">
-          <span class="author">by: <?php the_author(); ?> </span>
-          <span class="date"><?php the_date(); ?></span>
-          <span class="num-comments"><?php comments_number(); ?> </span>
+          <div class="postmeta">
+            <span class="author">by: <?php the_author(); ?> </span>
+            <span class="date"><?php the_date(); ?></span>
+            <span class="num-comments"><?php comments_number(); ?> </span>
+          </div>
         </div>
       </article>
         <!-- end .postmeta -->
@@ -41,7 +44,7 @@
   else{
   echo 'Sorry, no posts to show';
   } ?>
-</main>    
+</main>
 <!-- end of main#content ^_^  -->
   <?php get_sidebar();//including the sidebar :) ?>
   <?php get_footer();//including the footerrr :D ?>
