ad79e5cf9a3bc898cd51ac8ebb7c26069c6bb1ee
arielkreyes <ariel.k.reyes@hotmail.com>
2017-04-18 04:13:21 -0700

    0418
    Last Minute Tweaks

--- wp-content/themes/monster23/sidebar.php
+++ wp-content/themes/monster23/sidebar.php
@@ -1,14 +1,6 @@
 <aside id="sidebar">
   <!-- WIDGET AREA!
 if there are widgets show them, otherwisde do fallback content and don't forget to register_sidebar() in the functions.php -->
-  <section id="user_stuff" class="widget user">
-    <?php
-    $current_user =  wp_get_current_user();
-    if( is_user_logged_in()){
-      echo $current_user->user_firstname;
-    }
-     ?>
-  </section>
   <section id="search" class="widget">
     <?php get_search_form(); ?>
   </section>
@@ -20,16 +12,20 @@ if there are widgets show them, otherwisde do fallback content and don't forget 
   <section id="recentposts" class="widget">
     <h3 class="widgettitle">recent stories</h3>
     <?php
-    //show the most recent published posts
-    wp_get_recent_posts( array(
-      'numberposts' => 3,
-      'offset' => 0,
-      'category' => 0,
-      'orderby' => 'post_date',
-      'order' => 'DESC',
-      'post_type' =>  'post',
-      'post_status' =>  'publish, private',
-      'supress_filters' => true,
-    ), ARRAY_A ); ?>
+    global $post;
+    $args = array( 'posts_per_page' => 5, 'order'=> 'DESC', 'orderby' => 'date' );
+    $postslist = get_posts( $args );
+    foreach ( $postslist as $post ) :
+      setup_postdata( $post ); ?>
+    	<a href="<?php the_permalink(); ?>"><div>
+    		<h5><?php the_date(); ?></h5>
+    		<br />
+    		<p><?php the_title(); ?></p>
+    	</div>
+    </a>
+    <?php
+    endforeach;
+    wp_reset_postdata();
+    ?>
   </section>
 </aside>
